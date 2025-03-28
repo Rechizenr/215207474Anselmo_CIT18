3f158602d1a687f56ffa7aea171e08837911fb5b
Belmar Aselmo <echizenr96@gmail.com>
2025-03-28 10:10:14 +0800

    modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
     class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
     Dashboard
 </a>
-<a href="{{ route('notes') }}"
+<a href="{{ route('notes.index') }}"
    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 
           focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
     Notes
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@ Route::get('/', function () {
 
 // Notes CRUD routes
 Route::resource('notes', NoteController::class);
-Route::get('/notes', [NoteController::class, 'index'])->name('notes');
-Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
 
 // User Registration Routes
 Route::get('/register', [UserController::class, 'showRegistrationForm'])->name('register.form');
